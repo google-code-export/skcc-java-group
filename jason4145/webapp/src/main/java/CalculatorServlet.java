565c42691ee72a723ab624612f3bbb0bf8a989f0
jason4145@gmail.com <jason4145@gmail.com@56700096-5113-3b7f-fe4b-e1b176158b44>
2012-09-27 04:51:42 +0000

    과제 5번 제출

--- jason4145/webapp/src/main/java/CalculatorServlet.java
+++ jason4145/webapp/src/main/java/CalculatorServlet.java
@@ -1,6 +1,9 @@
 
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,6 +29,49 @@ public class CalculatorServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("CalculatorServlet DoGet!!!");
+		double num1 = Double.parseDouble(request.getParameter("num1"));
+		double num2 = Double.parseDouble(request.getParameter("num2"));
+		double result = 0;
+		String op = request.getParameter("operator");
+		
+		// for using counting digit
+		String result1 = null;
+		String num3 = request.getParameter("num1");
+		String num4 = request.getParameter("num2");
+		
+		// define digit variable
+		int num_cnt = 0;
+
+		// initiate digit count variable & check longer digit and setting digit count variable
+		if((num3.indexOf(".") == -1) && (num4.indexOf(".") == -1)){
+			num_cnt = 2;
+		}else{
+			if(num3.indexOf(".") != -1)
+				num_cnt = num3.length()-num3.indexOf(".")-1;
+			if(num4.indexOf(".") > num_cnt)
+				num_cnt = num4.length()-num4.indexOf(".")-1;
+		}
+		
+		// branch calculation
+		if(op.equals("/")){
+			result = (double) num1 / (double) num2;
+			// change result variable format double to string
+			NumberFormat nf = NumberFormat.getNumberInstance();
+			nf.setMaximumFractionDigits(num_cnt);
+			nf.setGroupingUsed(false);
+			result1 = nf.format(result);
+			request.setAttribute("result", result1);
+		}else{
+			if(op.equals("+")){
+				result = (double) num1 + (double) num2;
+			}else if(op.equals("-")){
+				result = (double) num1 - (double) num2;
+			}else if(op.equals("*")){
+				result = (double) num1 * (double) num2;
+			}
+			request.setAttribute("result", result);
+		}		
+		request.getRequestDispatcher("./index.jsp").forward(request, response);	
 	}
 
 	/**
@@ -55,12 +101,12 @@ public class CalculatorServlet extends HttpServlet {
 			}else{
 				result = num1 / num2;
 			}
-			request.setAttribute("operator", op);
+			
 			request.setAttribute("num1", num1);
 			request.setAttribute("num2", num2);
+			request.setAttribute("operator", op);
 			request.setAttribute("result", result);
 			request.getRequestDispatcher("/index.jsp").forward(request, response);
 		}
 	}
-
 }
--- jason4145/webapp/src/main/webapp/index.jsp
+++ jason4145/webapp/src/main/webapp/index.jsp
@@ -1,31 +1,22 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="UTF-8"%>
+<%@ page isELIgnored ="false" %>                   
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <body>
-<%	
-	request.setCharacterEncoding("UTF-8");
-	int result = 0;
-	int num1 = 0;
-	int num2 = 0;
-	String operator = "minus";
-	if(request.getAttribute("result") != null)
-		result = (Integer)request.getAttribute("result");
-	if(request.getAttribute("num1") != null)
-		num1 = (Integer)request.getAttribute("num1");
-	if(request.getAttribute("num2") != null)
-		num1 = (Integer)request.getAttribute("num2");
-%>
 <h1>Calculator Test</h1>
-<form method="POST" action="CalculatorServlet.do">
-	<input name="num1" id="num1" size="10" value=<%=num1 %> />
-	<select id="operator" name="operator" >
-		<option value="plus"> + </option>
-		<option value="minus"> - </option>
-		<option value="multi"> * </option>
-		<option value="divide"> / </option>
+	<form id="calcform" action="./CalculatorServlet.do">
+	<input name="num1" type="text" size="10" value="${param.num1}"/>
+	<select name="operator">
+		<option label="+" value="+" ${param.operator == '+' ? 'selected':''}>
+		<option label="-" value="-" ${param.operator == '-' ? 'selected':''}>
+		<option label="*" value="*" ${param.operator == '*' ? 'selected':''}>
+		<option label="/" value="/" ${param.operator == '/' ? 'selected':''}>
 	</select>
-	<input name="num2" id="num2" size="10" value=<%=num2 %> /> = <%=result %>
-	<br>
-	<input name="submit" type="submit">
+	<input name="num2" type="text" size="10" value="${param.num2}"/>
+	= ${result}
+	<br/>
+	<input type="submit" value="계산">
 </form>
 </body>
 </html>
